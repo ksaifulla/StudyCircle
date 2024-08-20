7cf565731941c09e26e698306e322e0be956d2dd
KhalidSaifulla0 <cavesontop0@gmail.com>
2024-08-20 16:25:50 +0530

    added signin routes

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -3,26 +3,73 @@ const express = require("express");
 const { User } = require("../db");
 const router = express.Router();
 const bycrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
 // User Routes
 router.post("/signup", async (req, res) => {
   // Implemented User signup logic
-  const username = req.body.username;
-  const password = req.body.password;
-  const existingUser = await User.findOne({
-    username: username,
-  });
-  if (!existingUser) {
-    const encodedPassword = await bycrypt.hash(password, 10);
-    await User.create({
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+    const existingUser = await User.findOne({
       username: username,
-      password: encodedPassword,
     });
-    res.json({
-      msg: "User created successfully",
+    if (!existingUser) {
+      const encodedPassword = await bycrypt.hash(password, 10);
+      await User.create({
+        username: username,
+        password: encodedPassword,
+      });
+      res.json({
+        msg: "User created successfully",
+      });
+    } else {
+      res.json({
+        msg: "User already exist",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      msg: "An error occurred during signup",
+      error: error.message,
     });
-  } else {
-    res.json({
-      msg: "User already exist",
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  // Implemented User login logic
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const existingUser = await User.findOne({ username: username });
+    if (existingUser) {
+      const validPassword = await bycrypt.compare(
+        password,
+        existingUser.password
+      );
+      if (!validPassword) {
+        return res.json({
+          msg: "your password is incorrect",
+        });
+      }
+      const token = jwt.sign(
+        {
+          username,
+        },
+        JWT_SECRET
+      );
+      return res.json({
+        token,
+      });
+    }
+    return res.json({
+      msg: "User does not exist",
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: "An error occurred during signin",
+      error: error.message,
     });
   }
 });
